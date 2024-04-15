006bfb723177e029fb8914beb51253f8dabc2f5d
D010X <167040044+D010X@users.noreply.github.com>
2024-04-14 20:29:47 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from app.functions import send_data
+from app.functions import send_data, retrivedata
 
 
 # Create your views here.
@@ -9,28 +9,15 @@ from app.functions import send_data
 def getData(request):
     shop_url = request.data.get('shop_url')
     access_token = request.data.get('access_token')
-    product_data = {
-        "title": "jeweleryssssssssssssssssss",
-        "body_html": "<p>This is a sample product description.</p>",
-        "vendor": "Sample Vendor",
-        "product_type": "Sample Type",
-        "variants": [
-            {
-                "option1": "Blue",
-                "price": "10.00",
-                "sku": "SAMPLE-BLUE"
-            },
-            {
-                "option1": "Red",
-                "price": "12.00",
-                "sku": "SAMPLE-RED"
-            }
-        ]
-    }
+    api_url = request.data.get('api')
+    user_id = request.data.get('userid')
+
+    product_data = retrivedata(api_url, user_id)
 
     response = send_data(product_data, shop_url, access_token)
     # Ensure that the status_code is an integer
     status_code = int(response.status_code)
 
     # Return the response with the correct status code
-    return Response(response.data, status=status_code)
+    return Response(response.data, status=status_code
+                    )
